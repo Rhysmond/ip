4ac8a21805d1220416310e8196acc0fe4f4b5ca4
Rhysmond <E1406512@u.nus.edu>
2025-08-19 10:50:03 +0800

    Level-0
    Added greeting
    Renamed bot to nyan-chan

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# NyanChan User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/NyanChan.java
@@ -0,0 +1,7 @@
+public class NyanChan {
+    public static void main(String[] args) {
+        String line_break = "____________________________________________________________\n";
+        System.out.println(line_break + "Hello! I'm NyanChan!\nWhat can I do for you?\n"
+        + line_break + "Bye. Hope to see you again soon!\n" + line_break);
+    }
+}
