379a256fa3802d750f624ca943aaabe0000ff4a6
Rhysmond <E1406512@u.nus.edu>
2025-08-19 12:24:46 +0800

    Level-1

--- src/main/java/NyanChan.java
+++ src/main/java/NyanChan.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class NyanChan {
     public static void main(String[] args) {
         String line_break = "____________________________________________________________\n";
-        System.out.println(line_break + "Hello! I'm NyanChan!\nWhat can I do for you?\n"
-        + line_break + "Bye. Hope to see you again soon!\n" + line_break);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(line_break + "Hello! I'm NyanChan!\nWhat can I do for you?\n" + line_break);
+
+        while (true) {
+            String user_input = scanner.nextLine();
+            if (user_input.equals("bye")) {
+                System.out.println(line_break + "Bye. Hope to see you again soon!\n" + line_break);
+                break;
+            } else {
+                System.out.println(line_break + user_input + "\n" + line_break);
+            }
+        }
     }
 }
