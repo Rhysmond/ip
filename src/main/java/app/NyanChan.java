a7862ffc2c0ac4fbb2feb775afd6f916f282b488
Rhysmond <E1406512@u.nus.edu>
2025-08-30 22:34:41 +0800

    Refactor NyanChan.java

--- src/main/java/app/NyanChan.java
+++ src/main/java/app/NyanChan.java
@@ -14,23 +14,33 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class NyanChan {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Ui ui = new Ui();
-        Storage storage = new Storage("./data/nyanchan.txt");
-        TaskList taskList = new TaskList();
+    private Storage storage;
+    private TaskList taskList;
+    private Ui ui;
 
+    public NyanChan(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
         try {
             taskList = new TaskList(storage.load());
         } catch (NyanException e) {
             ui.showError(e.getMessage());
+            taskList = new TaskList();
         }
+    }
 
+    public void run() {
         ui.showWelcome();
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String userInput = scanner.nextLine();
+            String userInput = scanner.nextLine().trim();
+
             try {
+                if (userInput.equals("bye")) {
+                    ui.showGoodbye();
+                    break;
+                }
                 Parser.handleCommand(userInput, taskList, ui, storage);
             } catch (NyanException e) {
                 ui.showError(e.getMessage() + "\n");
@@ -38,5 +48,11 @@ public class NyanChan {
                 ui.showError("Something went wrong: " + e.getMessage() + "\n");
             }
         }
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        new NyanChan("./data/nyanchan.txt").run();
     }
 }
